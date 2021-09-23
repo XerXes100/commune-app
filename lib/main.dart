715837acad9062e59fa371377a624aac92854d0c
XerXes100 <tirthjain1411@gmail.com>
2021-09-23 12:12:34 +0530

    Updates

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,6 @@ import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
--- lib/screens/signUp.dart
+++ lib/screens/signUp.dart
@@ -55,7 +55,6 @@ class _SignUpState extends State<SignUp> {
           builder: (context) => ChatList()
         ));
       });
-      
     }
   }
 
